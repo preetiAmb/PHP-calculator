4b264d4a7b9099a9b0a79047be382b00959f6525
Preeti <preetijav@gmail.com>
2023-07-10 16:48:07 +0200

    added addition, subtraction, multi and division

--- calculator.php
+++ calculator.php
@@ -1,22 +1,41 @@
 <?php
-// Check if the correct number of arguments are provided
-if (count($argv) != 3) {
-    echo "Usage: php calculator.php <num1> <num2>\n";
+
+if (count($argv) != 4) {
+    echo "Usage: php calculator.php <operator> <num1> <num2>\n";
     exit(1);
 }
 
-// Get the numbers from command line arguments
-$num1 = $argv[1];
-$num2 = $argv[2];
+$operator = $argv[1];
+$num1 = $argv[2];
+$num2 = $argv[3];
 
-// Validate the input as numbers
 if (!is_numeric($num1) || !is_numeric($num2)) {
     echo "Error: Both arguments must be numeric\n";
     exit(1);
 }
 
-// Multiply the numbers
-$result = $num1 * $num2;
+$result = 0;
+switch ($operator) {
+    case '+':
+        $result = $num1 + $num2;
+        break;
+    case '-':
+        $result = $num1 - $num2;
+        break;
+    case '*':
+        $result = $num1 * $num2;
+        break;
+    case '/':
+        
+        if ($num2 == 0) {
+            echo "Error: Division by zero\n";
+            exit(1);
+        }
+        $result = $num1 / $num2;
+        break;
+    default:
+        echo "Error: Invalid operator\n";
+        exit(1);
+}
 
-// Print the result
-echo "The result of multiplication is: " . $result . "\n";
+echo "The result is: " . $result . "\n";
